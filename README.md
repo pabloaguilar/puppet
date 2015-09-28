c5d5fff6fac95d40b4b79c75c1d71159bca70e25
lhchavez <lhchavez@lhchavez.com>
2015-09-28 00:39:29 -0700

    Importing whatever we had in the deploy repository

---
+++ README.md
@@ -0,0 +1,6 @@
+omegaUp Puppet module
+=====================
+
+This is a helper module to provide both Docker and Vagrant deployment.  Please
+refer to the [deploy](https://github.com/omegaup/deploy) repository for more
+information.
---
+++ manifests/minijail.pp
@@ -0,0 +1,19 @@
+class omegaup::minijail {
+	package { 'sudo':
+		ensure  => present,
+	}
+
+	package { 'omegaup-minijail':
+		ensure  => present,
+		require => Apt::Ppa['ppa:omegaup/omegaup'],
+	}
+
+	file { '/etc/sudoers.d/minijail':
+		ensure  => 'file',
+		source  => "puppet:///modules/omegaup/sudoers-minijail",
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0440',
+		require => [User['omegaup'], Package['sudo']],
+	}
+}
