7e1585a5ad686e2cc6fb0b077f5223e3c85b0700
lhchavez <lhchavez@lhchavez.com>
2018-01-08 02:46:28 +0000

    Backend refresh
    This enables the ephemeral grader.

--- manifests/services.pp
+++ manifests/services.pp
@@ -2,7 +2,7 @@
 class omegaup::services {
   remote_file { '/var/lib/omegaup/omegaup-backend.tar.xz':
     url      => 'https://omegaup-dist.s3.amazonaws.com/omegaup-backend.tar.xz',
-    sha1hash => 'dd0e1a01cba00f31fcb3ce088da68c65a198b4c8',
+    sha1hash => '88206a2aa80cdd646d831b2a14596bc3a666beb0',
     mode     => 644,
     owner    => 'root',
     group    => 'root',
