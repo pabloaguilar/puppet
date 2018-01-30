fec6eba4ff9d4c56ed1a7eba843eecea47cd2a07
lhchavez <lhchavez@lhchavez.com>
2018-01-30 04:18:07 +0000

    Backend refresh

--- manifests/services/runner.pp
+++ manifests/services/runner.pp
@@ -10,7 +10,7 @@ class omegaup::services::runner (
 
   remote_file { '/var/lib/omegaup/omegajail-xenial-distrib-x86_64.tar.bz2':
     url      => 'https://omegaup-omegajail.s3.amazonaws.com/omegajail-xenial-distrib-x86_64.tar.bz2',
-    sha1hash => '9213710a42e3550a9d80b7e5a1ac1caee017486a',
+    sha1hash => '475c386e58fc18cbe5818e04f31384c83292432f',
     mode     => 644,
     owner    => 'root',
     group    => 'root',
